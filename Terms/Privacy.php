39feee68230272dc9b5f8d81388d970da1a21a06
Bcript <91134167+Bcript@users.noreply.github.com>
2021-12-24 12:37:55 +0700

    Create Privacy.php

---
+++ Terms/Privacy.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=1170">
+        <meta name="description" content="Tired of having to go to shady websites to download an iso image? WinWRLDWide allows you to download operating systems (Note: We do not provide the product keys)">
+        <meta property="og:image" content="https://winwrldwide.xyz/assets/img/disc-image.png"/>
+        <meta property="og:type" content="website"/>
+        <meta name="keywords" content="WinWRLDWide, windows, iso, operating systems, mac, macintosh">
+        <title>Legal | WinWRLDWide</title>
+        <?php
+            require $_SERVER['DOCUMENT_ROOT'].'/inc/req.head.php';
+         ?>
+         <script>
+         $(document).ready(function(){
+           $('.ui .item').on('click', function() {
+             $('.ui .item').removeClass('active');
+             $(this).addClass('active');
+           });
+         });
+       </script>
+      </head>
+      <body>
+        
+<div id="app">
+           <?php
+           include_once $_SERVER['DOCUMENT_ROOT'].'/inc/inc.nav.php';
+            ?>
+            <section class="hero is-dark is-bold ui raised segment" style="margin-top:-20px; border: 0px !important; border-bottom-left-radius: 5px !important; border-bottom-right-radius: 5px !important; background: linear-gradient(90deg,#7E80BC,#79B5D9);">
+            <div class="hero-body is-bold">
+<div class="container has-text-centered">
+<h1 class="title is-size-3">Privacy Policy</h1>
+<h2 class="subtitle has-text-success">Effective as of March 29, 2020.</h2>
+</div>
+</div>
+</section>
+          <nav class="navbar ui tiny menu" style="margin-top:-20px; border: 0px !important; border-bottom-left-radius: 5px !important; border-bottom-right-radius: 5px !important;">
+  <div class="container is-centered">
+    <div class="navbar-tabs">
+      <a class="navbar-item is-tab is-primary" position="is-centered" href="/terms/" data-toggle="tab" class="item">
+        Terms
+      </a>
+      <a class="navbar-item is-primary is-tab is-active" position="is-centered" href="/terms/privacy" data-toggle="tab" class="item">
+        Privacy
+      </a>
+      <a class="navbar-item is-primary is-tab" position="is-centered" href="/terms/DMCA" data-toggle="tab" class="item">
+        DMCA
+      </a>
+    </div>
+  </div>
+</nav>
+    <div class="content" style="margin-top: 20px;">
+      <div class="columns is-fullheight">
+  <div class="column is-2 is-sidebar-menu">
+    <aside class="menu sticky">
+      <div id="app" class="container">
+        <b-menu>
+       <b-menu-list has-text-weight-bold label="Legal">
+    <div class="ui vertical text menu" style="webkit-box-shadow: 0 0px 0px 0 rgba(34,36,38,.15); box-shadow: 0 0px 0px 0 rgba(34,36,38,.15); ">
+  <div class="header item">Sections</div>
+  <a class="item" id="link-one" href="#collection">
+    &nbsp&nbsp What information do we collect?
+  </a>
+   <a class="item" id="link-two" href="#use">
+    &nbsp&nbsp What do we use your information for?
+  </a>
+  <a class="item" href="#protection">
+    &nbsp&nbsp How do we protect your information?
+  </a>
+  <a class="item" href="#cookies">
+    &nbsp&nbsp Do we use cookies?
+  </a>
+  <a class="item" href="#links">
+    &nbsp&nbsp Third party links
+  </a>
+  <a class="item" href="#policy">
+    &nbsp&nbsp Online Privacy Policy Only
+  </a>
+  <a class="item" href="#consent">
+    &nbsp&nbsp Your Consent
+  </a>
+<a class="item" href="#privacy">
+    &nbsp&nbsp Changes to our Privacy Policy
+  </a>
+</div>
+
+</b-menu-list>
+</b-menu>
+    </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="ui bottom fixed compact segment is-fixed-bottom" style="background: #fff;">
+  <a href="#top" role="button" class="sidebar-btt"><span class="icon is-small"><i class="mdi mdi-arrow-expand-up" style="bottom: 0;"></i></span> <span>Back to top</span></a>
+</div>
+</aside> 
+
+
+  </div>
+  <div class="column is-main-content">
+          <a name="terms"></a>
+    <div class="ui raised segment" style="margin-top:-20px; border-bottom-left-radius: 10px !important; border-bottom-right-radius: 10px !important; border-top-left-radius: 0px !important; border-top-right-radius: 0px !important; border-top: none;">
+      <div class="tab-content">
+      <div class="tab-pane active" id="terms">
+        <div class="segmentcontent">
+
+          <a name="collection"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+        What information do we collect?
+      </h3>
+
+<p>We collect information from you when you register on our site and gather data when you participate in the website by reading, writing, and evaluating the content shared here.</p>
+<p>When registering on our site, you may be asked to enter your name and e-mail address. You may, however, visit our site without registering. Your e-mail address will be verified by an email containing a unique link. If that link is visited, we know that you control the e-mail address.</p>
+<p>When registered and posting, we record the IP address that the post originated from. We also may retain server logs which include the IP address of every request to our server.</p>
+
+          <a name="use"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+    What do we use your information for?
+      </h3>
+<p>Any of the information we collect from you may be used in one of the following ways:</p>
+
+<li>•	To personalize your experience — your information helps us to better respond to your individual needs.</li>
+<li>•	To improve our site — we continually strive to improve our site offerings based on the information and feedback we receive from you.</li>
+<li>•	To improve customer service — your information helps us to more effectively respond to your customer service requests and support needs.</li>
+<li>•	To send periodic emails — The email address you provide may be used to send you information, notifications that you request about changes to topics or in response to your user name, respond to inquiries, and/or other requests or questions.</li>
+
+          <a name="protection"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+    How do we protect your information?
+      </h3>
+      
+<p>We implement a variety of security measures to maintain the safety of your personal information when you enter, submit, or access your personal information. <br>
+What is your data retention policy?</p>
+
+<p>We will make a good faith effort to:</p>
+
+<li>•	Retain server logs containing the IP address of all requests to this server no more than 90 days.</li>
+<li>•	Retain the IP addresses associated with registered users and their posts no more than 5 years.</li>
+
+          <a name="cookies"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+    Do we use cookies?
+      </h3>
+      
+<p>Yes. Cookies are small files that a site or its service provider transfers to your computer’s hard drive through your Web browser (if you allow). These cookies enable the site to recognize your browser and, if you have a registered account, associate it with your registered account.</p>
+<p>We use cookies to understand and save your preferences for future visits and compile aggregate data about site traffic and site interaction so that we can offer better site experiences and tools in the future. We may contract with third-party service providers to assist us in better understanding our site visitors. These service providers are not permitted to use the information collected on our behalf except to help us conduct and improve our business.</p>
+<p>Do we disclose any information to outside parties?</p>
+<p>We do not sell, trade, or otherwise transfer to outside parties your personally identifiable information. This does not include trusted third parties who assist us in operating our site, conducting our business, or servicing you, so long as those parties agree to keep this information confidential. We may also release your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others rights, property, or safety. However, non-personally identifiable visitor information may be provided to other parties for marketing, advertising, or other uses.</p>
+
+          <a name="links"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+    Third party links
+      </h3>
+      
+<p>Occasionally, at our discretion, we may include or offer third party products or services on our site. These third party sites have separate and independent privacy policies. We therefore have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our site and welcome any feedback about these sites.</p>
+<p>Children’s Online Privacy Protection Act Compliance</p>
+<p>Our site, products and services are all directed to people who are at least 13 years old or older. If this server is in the USA, and you are under the age of 13, per the requirements of COPPA (Children’s Online Privacy Protection Act), do not use this site.</p>
+
+          <a name="policy"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+    Online Privacy Policy Only
+      </h3>
+      
+<p>This online privacy policy applies only to information collected through our site and not to information collected offline.</p>
+
+          <a name="consent"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+    Your Consent
+      </h3>
+
+<p>By using our site, you consent to our web site privacy policy.</p>
+
+          <a name="policy"></a>
+<h3 style="font-family: 'Roboto', sans-serif;" id="two">
+    Changes to our Privacy Policy
+      </h3>
+      
+<p>If we decide to change our privacy policy, we will post those changes on this page.</p> <hr>
+<small>This document is CC-BY-SA. It was last updated May 31, 2013.</small>
+
+</div>
+     </div>
+   </div>
+     <div class="tab-pane" id="privacy">
+
+    </div>
+    <div class="tab-pane" id="dmca">
+                        </div>
+   </div>
+  </div>
+  </div>
+</div>
+    </div>
+</div>
+<script>
+        $(document).ready(function(){
+  $('.ui .item').on('click', function() {
+     $('.ui .item').removeClass('active');
+     $(this).addClass('active');
+  });
+});
+$(document).ready(function(){
+$('.is-tab').on('click', function() {
+$('.is-tab').removeClass('is-active');
+$(this).addClass('is-active');
+});
+});
+$(".openbtn").on("click", function() {
+  $(".ui.sidebar").toggleClass("very thin icon");
+  $(".asd").toggleClass("marginlefting");
+  $(".sidebar z").toggleClass("displaynone");
+  $(".ui.accordion").toggleClass("displaynone");
+  $(".ui.dropdown.item").toggleClass("displayblock");
+
+  $(".logo").find('img').toggle();
+
+})
+$(".ui.dropdown").dropdown({
+  allowCategorySelection: true,
+  transition: "fade down",
+  context: 'sidebar',
+  on: "hover"
+});
+
+$('.ui.accordion').accordion({
+  selector: {
+
+  }
+});
+
+$(window).scroll(function() { // when the page is scrolled run this
+    if($(this).scrollTop() != 0) { // if you're NOT at the top
+        $('#top').fadeIn("fast"); // fade in
+    } else { // else
+        $('#top').fadeOut("fast"); // fade out
+    }
+});
+$('.ui.dropdown')
+  .dropdown();
+
+        </script>
+<div class="bottomcontent">
+   <?php
+   include_once $_SERVER['DOCUMENT_ROOT'].'/inc/inc.footer.php';
+    ?>
+</div>
+      </body>
+</html>
